9ace4f2ea843a139aea93c12e29cbe4ef62a82df
BraveHeart <lzc978@126.com>
2019-02-21 15:58:38 +0800

    订单模块

--- meiduo/meiduo/apps/goods/serializers.py
+++ meiduo/meiduo/apps/goods/serializers.py
@@ -1,1 +1,36 @@
-from drf_haystack.serializers import HaystackSerializerfrom rest_framework import serializersfrom .models import SKUfrom .search_indexes import SKUIndexclass SKUSerializer(serializers.ModelSerializer):    """商品列表界面"""    class Meta:        model = SKU        fields = ['id', 'name', 'price', 'default_image_url', 'comments']class SKUSearchSerializer(HaystackSerializer):    """    SKU索引结果数据序列化器    """    object = SKUSerializer(read_only=True)    class Meta:        index_classes = [SKUIndex]        fields = ('text', 'object')
+from rest_framework import serializers
+
+from .models import SKU
+from orders.models import OrderInfo, OrderGoods
+
+
+class SKUInfoSerializer(serializers.ModelSerializer):
+    """订单商品详情"""
+    class Meta:
+        model = SKU
+        fields = ['id', 'name', 'default_image_url']
+
+
+class OrderGoodsSerializer(serializers.ModelSerializer):
+    """订单商品数据序列化器"""
+    sku = SKUInfoSerializer()
+
+    class Meta:
+        model = OrderGoods
+        fields = ['count', 'sku', 'price']
+
+
+class OrderInfoSerializer(serializers.ModelSerializer):
+    """订单信息数据序列化器"""
+    skus = OrderGoodsSerializer(many=True)
+
+    class Meta:
+        model = OrderInfo
+        fields = ['status', 'pay_method', 'total_amount', 'freight', 'create_time', 'order_id', 'skus']
+
+
+class SKUSerializer(serializers.ModelSerializer):
+    """商品列表界面"""
+    class Meta:
+        model = SKU
+        fields = ['id', 'name', 'price', 'default_image_url', 'comments']
--- meiduo/meiduo/apps/goods/views.py
+++ meiduo/meiduo/apps/goods/views.py
@@ -1,37 +1,70 @@
 from django.shortcuts import render
-from drf_haystack.viewsets import HaystackViewSet
-from rest_framework.generics import ListAPIView
 from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 
-from .models import SKU
-from .serializers import SKUSerializer, SKUSearchSerializer
-
-
+from goods.models import SKU
+from orders.models import OrderInfo, OrderGoods
+from goods.serializers import SKUSerializer, OrderInfoSerializer
 # Create your views here.
-# /categories/(?P<category_id>\d+)/skus?page=xxx&page_size=xxx&ordering=xxx
+from users.models import User
 
-class SKUListView(ListAPIView):
-    """商品列表界面"""
 
-    # 指定序列化器
-    serializer_class = SKUSerializer
-    # 指定过滤后端为排序
-    filter_backends = [OrderingFilter]
-    # 指定排序字段
-    ordering_fields = ['create_time', 'price', 'sales']
-
-    # 指定查询集
-    # queryset = SKU.objects.filter(is_launched=True, category_id=category_id)
+class UserInfoOrderView(ListAPIView):
+    """个人订单"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrderInfoSerializer
+    # queryset = OrderInfo.objects.all()
 
     def get_queryset(self):
-        category_id = self.kwargs.get('category_id')  # 获取url路径中的正则组别名提取出来的参数
-        return SKU.objects.filter(is_launched=True, category_id=category_id)
+        user = self.request.user
+        orders = OrderInfo.objects.filter(user_id=user.id)
+        for order in orders:
+            order.create_time = order.create_time.strftime('%Y-%m-%d %H:%M:%S')
+        return orders
+
+    # pagination_class = PageNumberPagination
 
 
-class SKUSearchViewSet(HaystackViewSet):
+# class UserInfoOrderView(APIView):
+#     """个人订单"""
+#     permission_classes = [IsAuthenticated]
+#     # serializer_class = UserCenterOrderSerializer
+#
+#     def get(self, request):
+#         user = request.user
+#         orders = user.orderinfo_set.all()
+#         order_id_list = []
+#         skus = []
+#         for order in orders:
+#             order_id_list.append(order)
+#             goods = order.skus.all()
+#             for good in goods:
+#                 skus.append(good.sku)
+#         data = {'results': order_id_list}
+#         # print(serializer.data)
+#         return Response(data=data)
+
+    # def get(self, request):
+    #     user_id = request.user.id
+    #
+    #     orders = User.objects.get(id=user_id)
+    #     serializer = UserCenterOrderSerializer({'orders': orders})
+    #     return Response(serializer.data)
+
+
+class SKUListView(ListAPIView):
     """
-    SKU搜索
+    sku列表数据
     """
-    index_models = [SKU]
+    serializer_class = SKUSerializer
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ('create_time', 'price', 'sales')
 
-    serializer_class = SKUSearchSerializer
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return SKU.objects.filter(category_id=category_id, is_launched=True)
